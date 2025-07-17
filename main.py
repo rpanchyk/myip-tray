f3592a1410c11ef1980fb1f676f1df3c64a3434f
rpanchyk <ruslan.panchik@gmail.com>
2025-07-18 01:07:59 +0300

    Big clean up

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import ctypes
 import os
 import sys
 import threading
-import time
 import random
-
-import requests
 import darkdetect
 from tkinter import Tk, Label
 from PIL import Image, ImageTk
-from idlelib.tooltip import Hovertip
 from pystray import Icon, MenuItem
 from resolvers.ip_api import IpApiResolver
 from resolvers.myip import MyIpResolver
@@ -25,117 +20,6 @@ def resource_path(relative_path):
 REQUEST_TIMEOUT = 5
 IMG_DIR = resource_path("assets/images")
 PIRATE_FLAG = f"{IMG_DIR}/pirate_flag.png"
-
-
-class Application:
-    def __init__(self):
-        self.stop_program = False
-        self.state = 0
-        self.last_ip = None
-
-        self.root = Tk()
-        self.root.overrideredirect(True)
-
-        self.root.title("MyIP Widget")
-        self.root.iconbitmap(f"{IMG_DIR}\\icon.ico")
-
-        self.detect_theme()
-
-        self.lab1 = Label(self.root, bg=self.bg_color)
-        self.lab1.bind("<Button-3>", self.hide_window)
-        self.lab2 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
-        self.lab3 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
-
-        self.lab1.grid(row=1, column=1)
-        self.lab2.grid(row=2, column=1)
-        self.lab3.grid(row=3, column=1)
-
-        Hovertip(self.lab1, 'right click to close')
-
-        self.icon = Icon("ping")
-        self.icon.icon = Image.open(PIRATE_FLAG)
-        self.icon.run_detached()
-        self.icon.menu = (
-            MenuItem('Quit', lambda: self.quit_window()),
-            MenuItem('Show', self.show_window)
-        )
-        # self.root.wm_attributes("-transparentcolor", self.bg_color)
-        self.root.attributes("-alpha", 0.7)
-        self.root.attributes('-topmost', True)
-        self.root.configure(bg=self.bg_color)
-        self.root.bind("<B1-Motion>", self.move_window)
-
-        self.thread2 = threading.Thread(target=self.update_data)
-        self.thread2.start()
-
-    def detect_theme(self):
-        theme = darkdetect.theme()
-
-        if theme == "Dark":
-            self.bg_color = "black"
-            self.fg_color = "white"
-        else:
-            self.bg_color = "white"
-            self.fg_color = "black"
-
-    def move_window(self, event):
-        self.root.geometry(f'+{event.x_root}+{event.y_root}')
-
-    def quit_window(self):
-        self.stop_program = True
-        self.icon.icon = None
-        self.icon.title = None
-        self.icon.stop()
-        self.root.destroy()
-
-    def show_window(self):
-        self.root.after(0, self.root.deiconify())
-
-    def hide_window(self, event):
-        self.root.withdraw()
-
-    def find_ip(self):
-        try:
-            req = requests.get("http://ip-api.com/json/", timeout=REQUEST_TIMEOUT)
-            if req.status_code == 200:
-                ip_data = req.json()
-                return ip_data
-            else:
-                return False
-        except Exception as e:
-            print(e)
-            return False
-
-    def update_data(self):
-        while not self.stop_program:
-            ip = self.find_ip()
-            if ip:
-                ip_address = ip["query"]
-                if ip_address != self.last_ip:
-                    self.last_ip = ip_address
-                    self.lab1.image = ImageTk.PhotoImage(image=Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png"))
-                    self.lab1.config(image=self.lab1.image)
-                    self.lab2.config(text=ip["country"])
-                    self.lab3.config(text=ip_address)
-                    self.icon.icon = Image.open(f"{IMG_DIR}\\flags\\{ip['countryCode']}.png")
-                    self.icon.title = ip_address
-            else:
-                self.last_ip = None
-                self.lab1.image = ImageTk.PhotoImage(file=PIRATE_FLAG)
-                self.lab1.config(image=self.lab1.image)
-                self.lab2.config(text="No Internet")
-                self.lab3.config(text="")
-                self.icon.icon = Image.open(PIRATE_FLAG)
-            time.sleep(5)
-
-    def run(self):
-        try:
-            ctypes.windll.shcore.SetProcessDpiAwareness(1)  # if your windows version >= 8.1
-        except:
-            ctypes.windll.user32.SetProcessDPIAware()  # win 8.0 or less
-
-        self.root.mainloop()
-        os._exit(1)
 
 
 class Application2:
@@ -167,8 +51,6 @@ class Application2:
         self.lab3.grid(row=3, column=1)
 
         self.lab1.bind("<Button-3>", self.hide_window)
-
-        # Hovertip(self.lab1, 'Right click to hide')
 
         self.icon = Icon("ping")
         self.icon.icon = Image.open(PIRATE_FLAG)
@@ -216,11 +98,6 @@ class Application2:
         self.x_last = event.x_root
         self.y_last = event.y_root
 
-        # ip_info = self.ipApiResolver.get()
-        # print(ip_info)
-        # ip_info = self.myIpResolver.get()
-        # print(ip_info)
-
     def move_window(self, event):
         x_delta = event.x_root - self.x_last
         y_delta = event.y_root - self.y_last
@@ -235,10 +112,6 @@ class Application2:
         for item in self.icon.menu:
             menu_items.append(MenuItem('Hide', self.hide_window) if str(item) == "Show" else item)
         self.icon.menu = menu_items
-        # self.icon.menu = (
-        #     MenuItem('Hide', self.hide_window),
-        #     MenuItem('Quit', self.quit_window)
-        # )
         self.icon.update_menu()
 
         self.root.deiconify()
@@ -248,10 +121,6 @@ class Application2:
         for item in self.icon.menu:
             menu_items.append(MenuItem('Show', self.show_window) if str(item) == "Hide" else item)
         self.icon.menu = menu_items
-        # self.icon.menu = (
-        #     MenuItem('Show', self.show_window),
-        #     MenuItem('Quit', self.quit_window)
-        # )
         self.icon.update_menu()
 
         self.root.withdraw()
@@ -299,12 +168,6 @@ class Application2:
                 resolvers_idx.remove(idx)
             else:
                 return ip_info
-
-        # for resolver in self.resolvers:
-        #     ip_info = resolver.get()
-        #     if not ip_info.is_empty():
-        #         return ip_info
-
         return IpInfo(None, None)
 
     def run(self):
@@ -313,5 +176,4 @@ class Application2:
 
 
 if __name__ == '__main__':
-    # Application().run()
     Application2().run()
