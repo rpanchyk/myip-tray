0e2b29ad00c4ca292c8951480eded99afadc0b09
rpanchyk <ruslan.panchik@gmail.com>
2025-07-18 14:02:12 +0300

    Remove darkdetect and resize window

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import sys
 import threading
 import random
-import darkdetect
 from tkinter import Tk, Label
 from PIL import Image, ImageTk
 from pystray import Icon, MenuItem
@@ -12,65 +11,71 @@ from models.ip_info import IpInfo
 
 
 def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):  # If running in a PyInstaller bundle
+    if hasattr(sys, "_MEIPASS"):  # If running in a PyInstaller bundle
         return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath('.'), relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 
-IMG_DIR = resource_path("assets/images")
-PIRATE_FLAG = f"{IMG_DIR}/pirate_flag.png"
+ICONS_DIR = resource_path("assets/icons")
+IMAGES_DIR = resource_path("assets/images")
+PIRATE_FLAG = f"{IMAGES_DIR}/pirate_flag.png"
 
 
-class Application2:
+class Application:
     def __init__(self):
         # Settings
         self.start_minimized = False
         self.on_top = True
-        self.position_x = 1830
+        self.position_x = 1810
         self.position_y = 890
-        self.refresh_interval_seconds = 5
-        self.refresh_timeout_seconds = 5
+        self.background_color = "aliceblue"  # https://www.plus2net.com/python/tkinter-colors.php
+        self.foreground_color = "black"
+        self.font_family = "Arial"
+        self.font_size = 11
+        self.refresh_interval_seconds = 10
+        self.refresh_timeout_seconds = 10
 
+        # Runtime
         self.x_last = 0
         self.y_last = 0
         self.last_ip = None
+        self.resolvers = [
+            IpApiResolver(self.refresh_timeout_seconds),
+            MyIpResolver(self.refresh_timeout_seconds)
+        ]
 
         self.root = Tk()
         self.root.overrideredirect(True)
-        self.root.iconbitmap(f"{IMG_DIR}\\icon.ico")
-
-        if darkdetect.theme() == "Dark":
-            self.bg_color = "grey"
-            self.fg_color = "white"
-        else:
-            self.bg_color = "white"
-            self.fg_color = "black"
-
-        self.root.attributes('-topmost', self.on_top)
-        self.root.configure(bg=self.bg_color)
+        self.root.iconbitmap(f"{ICONS_DIR}\\icon.ico")
+        self.root.attributes("-topmost", self.on_top)
+        self.root.configure(bg=self.background_color)
 
         self.root.bind("<ButtonPress-1>", self.on_left_button_press)
         self.root.bind("<B1-Motion>", self.move_window)
         self.root.bind("<Button-2>", self.quit_window)
 
-        self.lab1 = Label(self.root, bg=self.bg_color)
-        self.lab2 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
-        self.lab3 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0)
+        self.lab1 = Label(self.root, bd=0, borderwidth=0, bg=self.background_color)
+        self.lab2 = Label(self.root, bd=0, borderwidth=0,
+                          bg=self.background_color, fg=self.foreground_color)
+        self.lab3 = Label(self.root, bd=0, borderwidth=0,
+                          bg=self.background_color, fg=self.foreground_color)
 
-        self.lab1.grid(row=1, column=1)
-        self.lab2.grid(row=2, column=1)
-        self.lab3.grid(row=3, column=1)
+        # self.lab1.grid(row=1, column=1)
+        # self.lab2.grid(row=2, column=1)
+        # self.lab3.grid(row=3, column=1)
+        self.lab1.pack(expand=True)
+        self.lab2.pack(expand=True)
+        self.lab3.pack(expand=True)
 
         self.lab1.bind("<Button-3>", self.hide_window)
 
-        self.icon = Icon("ping")
+        self.icon = Icon("myip")
         self.icon.icon = Image.open(PIRATE_FLAG)
         self.icon.run_detached()
         self.icon.menu = (
-            MenuItem('Show', None),
-            MenuItem('Quit', self.quit_window)
+            MenuItem("Show", None),
+            MenuItem("Quit", self.quit_window)
         )
-        self.icon.update_menu()
 
         self.render_window(IpInfo.unknown())
         self.relocate_window()
@@ -80,10 +85,6 @@ class Application2:
         else:
             self.show_window(None)
 
-        self.resolvers = [
-            IpApiResolver(self.refresh_timeout_seconds),
-            MyIpResolver(self.refresh_timeout_seconds)
-        ]
         self.event = threading.Event()
         self.thread = threading.Thread(target=self.update_data)
         self.thread.start()
@@ -95,23 +96,40 @@ class Application2:
     def render_window(self, ip_info):
         if ip_info.is_unknown():
             self.last_ip = None
-            self.lab1.image = ImageTk.PhotoImage(file=PIRATE_FLAG)
+            img = Image.open(PIRATE_FLAG)
+
+            self.icon.icon = img
+            self.icon.title = "Unknown IP"
+
+            self.lab1.image = ImageTk.PhotoImage(image=img)
             self.lab1.config(image=self.lab1.image)
-            self.lab2.config(text="No connection")
-            self.lab3.config(text="...")
-            self.icon.icon = Image.open(PIRATE_FLAG)
+            self.lab2.config(text="Unknown IP", font=(self.font_family, self.font_size))
+            self.lab3.config(text="...", font=(self.font_family, self.font_size))
         else:
             if ip_info.ip_address != self.last_ip:
                 self.last_ip = ip_info.ip_address
-                self.lab1.image = ImageTk.PhotoImage(image=Image.open(f"{IMG_DIR}\\flags\\{ip_info.country_code}.png"))
-                self.lab1.config(image=self.lab1.image)
-                self.lab2.config(text=ip_info.country_code)
-                self.lab3.config(text=ip_info.ip_address)
-                self.icon.icon = Image.open(f"{IMG_DIR}\\flags\\{ip_info.country_code}.png")
+                img = Image.open(f"{IMAGES_DIR}\\flags\\{ip_info.country_code}.png")
+
+                self.icon.icon = img
                 self.icon.title = ip_info.ip_address
 
+                self.lab1.image = ImageTk.PhotoImage(image=img)
+                self.lab1.config(image=self.lab1.image)
+                self.lab2.config(text=ip_info.country_code, font=(self.font_family, self.font_size))
+                self.lab3.config(text=ip_info.ip_address, font=(self.font_family, self.font_size))
+
+                self.root.update_idletasks()
+                width = self.lab3.winfo_reqwidth() + 5
+                height = self.root.winfo_height()
+                self.root.geometry(f"{width}x{height}")
+
+                # original_image = Image.open(f"{IMAGES_DIR}\\flags\\{ip_info.country_code}.png")
+                # resized_image = original_image.resize((width - 10, original_image.height))
+                # self.lab1.image = ImageTk.PhotoImage(resized_image)
+                # self.lab1.config(image=self.lab1.image)
+
     def relocate_window(self):
-        self.root.update_idletasks()  # Ensure dimensions are updated
+        self.root.update_idletasks()
         width = self.root.winfo_width()
         height = self.root.winfo_height()
         x = self.position_x
@@ -130,7 +148,7 @@ class Application2:
     def show_window(self, event):
         menu_items = []
         for item in self.icon.menu:
-            menu_items.append(MenuItem('Hide', self.hide_window) if str(item) == "Show" else item)
+            menu_items.append(MenuItem("Hide", self.hide_window) if str(item) == "Show" else item)
         self.icon.menu = menu_items
         self.icon.update_menu()
 
@@ -139,7 +157,7 @@ class Application2:
     def hide_window(self, event):
         menu_items = []
         for item in self.icon.menu:
-            menu_items.append(MenuItem('Show', self.show_window) if str(item) == "Hide" else item)
+            menu_items.append(MenuItem("Show", self.show_window) if str(item) == "Hide" else item)
         self.icon.menu = menu_items
         self.icon.update_menu()
 
@@ -180,5 +198,5 @@ class Application2:
         os._exit(1)
 
 
-if __name__ == '__main__':
-    Application2().run()
+if __name__ == "__main__":
+    Application().run()
